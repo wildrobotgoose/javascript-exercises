9d9ed35ce9b6aa56e07d3808925f88093990a33a
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-07-30 20:18:20 -0400

    02 Implement repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return 'ERROR';
+    }
+    let repeatedString = '';
+    for (let i = 0; i < numberOfTimes; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
