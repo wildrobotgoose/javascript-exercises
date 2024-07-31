8191caae0a6963b87862a309697c9c96719df652
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-07-30 21:05:37 -0400

    04 Implement removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    const toRemove = new Array(array.length);
+    for (let i = 0; i < toRemove.length; i++) {
+        toRemove[i] = false;
+    }
+
+    for (const value of values) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === value) {
+                toRemove[i] = true;
+            }
+        }
+    }
+
+    const arrayWithElementsRemoved = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!toRemove[i]) {
+            arrayWithElementsRemoved.push(array[i]);
+        }
+    }
+
+    return arrayWithElementsRemoved;
 };
 
 // Do not edit below this line
