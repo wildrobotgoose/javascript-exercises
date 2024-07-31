b097b4cdb3eaf570d3c93ca7854d033b6ae32350
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-07-30 21:43:25 -0400

    07 Implement temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const roundToOneDecimal = function(number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5 / 9;
+
+  return roundToOneDecimal(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = 9 * celsius / 5 + 32;
+
+  return roundToOneDecimal(fahrenheit);
 };
 
 // Do not edit below this line
