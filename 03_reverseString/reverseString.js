81e7fdb2651c7e9b9b4ca8c9f7dc2bfefbceb872
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-07-30 20:42:50 -0400

    03 Implement reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = '';
+    for (let i = string.length; i > 0; i--) {
+        reversedString += string.substring(i-1, i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
