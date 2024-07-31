bc11d5890e95441e7801c890d84128d0dc0f4a00
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-07-30 21:25:38 -0400

    05 Use Number.isInteger to validate input

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,8 @@
 const sumAll = function(start, end) {
-    if (typeof start != 'number' || typeof end != 'number') {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
         return 'ERROR';
     }
     if (start < 0 || end < 0) {
-        return 'ERROR';
-    }
-    if (start % 1 != 0 || end % 1 != 0) {
         return 'ERROR';
     }
     return (start + end) * (Math.abs(start - end) + 1) / 2;
