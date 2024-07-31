3798b55fc1518bc856ddabaf8d9f6c1a16aef9f7
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-07-30 21:23:26 -0400

    05 Implement sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start != 'number' || typeof end != 'number') {
+        return 'ERROR';
+    }
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    if (start % 1 != 0 || end % 1 != 0) {
+        return 'ERROR';
+    }
+    return (start + end) * (Math.abs(start - end) + 1) / 2;
 };
 
 // Do not edit below this line
