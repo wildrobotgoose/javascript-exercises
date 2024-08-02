02c40d73079495dcebe5843ffe6a1da00ea06bad
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-08-02 14:28:22 -0400

    09 Implement palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const arr = Array.from(s.toLowerCase()).filter( (c) => {
+        return (c >= "a" && c <= "z") || (c >= "0" && c <= "9");
+    });
 
+    for (let i = 0; i < arr.length / 2; i++) {
+        if (arr[i] != arr[arr.length-1-i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
