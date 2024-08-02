a3e321a56a6316cd4d0ce6124c6972f1b63a05a4
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-08-02 14:38:46 -0400

    10 Implement fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
 
+    let [a, b] = [1, 1]
+
+    for (let i = 3; i <= n; i++) {
+        [a, b] = [b, a+b]
+    }
+
+    return b;
 };
 
 // Do not edit below this line
