d52079e151c7df6134f454729b4fb24f7ae2edf0
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-08-02 15:00:16 -0400

    12 Implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    const currentYear = new Date().getFullYear();
 
+    return (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) {
+
+    return people.reduce( (a, b) => getAge(a) >= getAge(b) ? a : b );
 };
 
 // Do not edit below this line
